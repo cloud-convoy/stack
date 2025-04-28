af6bc99b97e6573814b99ba0620e6497b1b6b86d
jasonwalsh <2184329+jasonwalsh@users.noreply.github.com>
2025-04-28 17:04:10 -0400

    chore: move data source to dedicated file

--- data.tf
+++ data.tf
@@ -1,3 +1,12 @@
 data "aws_caller_identity" "this" {}
 
 data "aws_region" "this" {}
+
+data "aws_default_tags" "this" {
+  lifecycle {
+    postcondition {
+      condition     = can(regex("[a-zA-Z][-a-zA-Z0-9]*", self.tags["Name"]))
+      error_message = "Member must satisfy regular expression pattern: [a-zA-Z][-a-zA-Z0-9]*"
+    }
+  }
+}
--- providers.tf
+++ providers.tf
@@ -8,12 +8,3 @@ provider "aws" {
     }
   }
 }
-
-data "aws_default_tags" "this" {
-  lifecycle {
-    postcondition {
-      condition     = can(regex("[a-zA-Z][-a-zA-Z0-9]*", self.tags["Name"]))
-      error_message = "Member must satisfy regular expression pattern: [a-zA-Z][-a-zA-Z0-9]*"
-    }
-  }
-}
